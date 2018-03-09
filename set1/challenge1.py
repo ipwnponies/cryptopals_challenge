56f7f5e637c6cf320fa3a296153eb910885eec82
John Nguyen <ipwnponies@gmail.com>
2018-03-08 19:38:44 -0800

    Add solution to set 1, challenge 1

---
+++ set1/challenge1.py
@@ -0,0 +1,9 @@
+# Convert the hex string: 49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d
+# to base64 string: SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t
+import codecs
+
+input_string = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+hex_value = codecs.decode(input_string, 'hex')
+base64_value = codecs.encode(hex_value, 'base64')
+
+assert str(base64_value, 'utf-8').strip() == 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
