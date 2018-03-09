777ae393cf2c110e3707c7f6f3a21f7f16100816
John Nguyen <ipwnponies@gmail.com>
2018-03-08 19:43:51 -0800

    Add solution to set 1, challenge 2

---
+++ set1/challenge2.py
@@ -0,0 +1,11 @@
+import codecs
+
+input_string = '1c0111001f010100061a024b53535009181c'
+xor_string = '686974207468652062756c6c277320657965'
+value1 = codecs.decode(input_string, 'hex')
+value2 = codecs.decode(xor_string, 'hex')
+
+result = bytes(a ^ b for a, b in zip(value1, value2))
+hex_result = codecs.encode(result, 'hex')
+
+assert str(hex_result, 'utf-8') == '746865206b696420646f6e277420706c6179'
