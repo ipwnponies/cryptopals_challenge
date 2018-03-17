dbded8a0c48cc7944561285bc91d3fd8f6ef33b7
John Nguyen <ipwnponies@gmail.com>
2018-03-16 18:54:56 -0700

    Solution to set 1 challenge 4

--- set1/challenge3.py
+++ set1/challenge3.py
@@ -3,19 +3,25 @@
 # Also, what is the message
 import codecs
 
-input_string = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
-hex_value = codecs.decode(input_string, 'hex')
-
 
 def score_english(message):
     common_letters = 'ETAOIN SHRDLU'
     score = sum(1 for i in message if chr(i).upper() in common_letters)
     return score
 
-results = []
-# Byte key size range is 256 possible keys
-for key in range(256):
-    candidate = bytes([i ^ key for i in hex_value])
-    results.append((score_english(candidate), candidate))
 
-print(str(max(results)[1], 'utf8'))
+def brute_force_byte_key(message):
+    results = []
+    # Byte key size range is 256 possible keys
+    for key in range(256):
+        candidate = bytes([i ^ key for i in message])
+        results.append((score_english(candidate), candidate))
+    return results
+
+
+if __name__ == '__main__':
+    input_string = '1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736'
+    hex_value = codecs.decode(input_string, 'hex')
+    decrypted_candidates = brute_force_byte_key(hex_value)
+
+    print(str(max(decrypted_candidates)[1], 'utf8'))
