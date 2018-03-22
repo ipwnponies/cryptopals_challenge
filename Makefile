f42280b031b463f5d0136fe646f74441a1814dc9
John Nguyen <ipwnponies@gmail.com>
2018-03-22 01:22:57 -0700

    Add solution to set 1 challenge 7

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
-challenges:= 1 2 3 4 5 6
+challenges:= 1 2 3 4 5 6 7
 .DEFAULT_GOAL:= test
 
 .PHONY: venv
 venv:
-	./bin/venv_update.py venv= -ppython3 venv install= -r requirements-dev.txt --quiet
+	./bin/venv_update.py venv= -ppython3 venv install= -r requirements-dev.txt -r requirements.txt --quiet
 
 .PHONY: test
 test: $(challenges)
---
+++ set1/challenge7.py
@@ -0,0 +1,22 @@
+import codecs
+import os
+
+from Crypto.Cipher import AES
+
+from util import decrypt_rotating_bytekey
+
+KEY = 'YELLOW SUBMARINE'
+
+
+def main():
+    with open(os.path.join(os.path.dirname(__file__), 'challenge7_data')) as data_set:
+        input_text = data_set.read()
+    message = codecs.decode(input_text.encode('utf8'), 'base64')
+    cipher = AES.new(KEY, AES.MODE_ECB)
+    decrypted = cipher.decrypt(message)
+
+    assert decrypted.startswith("I'm back and I'm ringin' the bell")
+
+
+if __name__ == '__main__':
+    main()
