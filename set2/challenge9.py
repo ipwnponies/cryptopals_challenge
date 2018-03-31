7c4bb30938e6bb46395a591f3939b4b881955ec2
John Nguyen <ipwnponies@gmail.com>
2018-03-31 11:51:37 -0700

    Change pkcs padding to work only on bytes

--- set2/challenge9.py
+++ set2/challenge9.py
@@ -4,12 +4,12 @@ from util import chunk
 def pkcs_padding(message, chunk_size):
     '''Pad the message at the end with \x04 characters to get even block sizes.'''
     chunked = chunk(message, chunk_size)
-    chunked[-1] = '{message:\x04<{chunk_size}}'.format(message=chunked[-1], chunk_size=chunk_size)
-    return ''.join(chunked)
+    chunked[-1] = chunked[-1].ljust(chunk_size, b'\x04')
+    return b''.join(chunked)
 
 
 def main():
-    assert pkcs_padding('YELLOW SUBMARINE', 20) == 'YELLOW SUBMARINE\x04\x04\x04\x04'
+    assert pkcs_padding(b'YELLOW SUBMARINE', 20) == b'YELLOW SUBMARINE\x04\x04\x04\x04'
 
 
 if __name__ == '__main__':
