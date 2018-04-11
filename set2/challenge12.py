9b07a576b24e567862c1117467d70fe2744e178f
John Nguyen <ipwnponies@gmail.com>
2018-04-11 11:28:18 -0700

    Detect blocksize of encryption function

--- set2/challenge12.py
+++ set2/challenge12.py
@@ -4,6 +4,7 @@ from Crypto.Cipher import AES
 
 from set2.challenge11 import generate_random_bytes
 from set2.challenge9 import pkcs_padding
+from util import chunk
 
 
 def encryption_oracle(message, key):
@@ -21,9 +22,41 @@ def encryption_oracle(message, key):
     return cipher.encrypt(message)
 
 
+def detect_block_size(key):
+    '''Detect blocksize of encryption function.
+
+    Determines the block size used in encryption scheme. It passes in known common plaintext and
+    determines when the output blocks begin to repeat.
+    '''
+    blocksize = 0
+    for pad_length in range(1, 2**6):
+        ciphertext = encryption_oracle(b'a' * pad_length, key)
+
+        # Split the cipher text into blocks of even size
+        for j in range(len(ciphertext)//2, 1, -1):
+
+            # Skip non-divisible sizes
+            if len(ciphertext) % j != 0:
+                continue
+
+            chunks = chunk(ciphertext, j)
+            if len(chunks) != len(set(chunks)):
+                # If there are repeated blocks, this is a candiate size
+                blocksize = j
+                break
+        if blocksize:
+            break
+    else:
+        raise Exception('Could not detect block size')
+
+    return blocksize
+
+
 def main():
     key = generate_random_bytes(16)
-    print(key)
+
+    blocksize = detect_block_size(key)
+    assert blocksize == 16, 'ECB uses 16 byte blocks, detected blocksize of {}'.format(blocksize)
 
 
 if __name__ == '__main__':
